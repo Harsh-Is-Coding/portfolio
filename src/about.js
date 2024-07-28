8982c4c0255132badcbee7e6019cf5f1a015130c
Mellostar25 <67835601+Mellostar25@users.noreply.github.com>
2024-07-27 21:54:00 -0400

    about section csss

--- src/about.js
+++ src/about.js
@@ -1,17 +1,34 @@
+import headShot from "./head-shot.jpg"
+import './index.css'
+
 function About(){
     return (
         <div className="about">
-            <h1>About Me</h1>
-            <p>stuff about me</p>
-            <h3>McMaster Univsersity - Software Engineering</h3>
-            <h1>My Skills</h1>
-            <ul>
-                <li>React.Js</li>
-                <li>C++</li>
-                <li>Python</li>
-                <li>Postgres SQL</li>
-                <li>Tailwind CSS</li>
-            </ul>
+            <div className="aboutHead">
+                <h1 className="name">Hi, I'm Harsh Shukla <span className="wave"> 👋🏻</span></h1>
+                <img src={headShot} className="meImage" />
+            </div>
+            <div className="aboutContent">
+                <p className="abourPara">LET ME INTRODUCE MYSELF
+                    <br/><br/>
+I fell in love with programming, and I've learned quite a bit along the way! 😊
+<br/><br/>
+I am fluent in classics like Python, C++, and JavaScript.
+<br/><br/>
+My fields of interest include building innovative web technologies and products, AI and Cloud technologies.
+<br/><br/>
+Whenever possible, I apply my passion for developing products using React.js and modern tools like AWS and Postgres.</p>
+                <h3 className="university">McMaster Univsersity - Software Engineering</h3>
+                <h1 className="skillHead">My Skills</h1>
+                <ul className="skillList">
+                    <li className="skillItem">React.Js</li>
+                    <li className="skillItem">C++</li>
+                    <li className="skillItem">Python</li>
+                    <li className="skillItem">Postgres SQL</li>
+                    <li className="skillItem">Tailwind CSS</li>
+                </ul>
+            </div>
+            
         </div>
     )
 };
