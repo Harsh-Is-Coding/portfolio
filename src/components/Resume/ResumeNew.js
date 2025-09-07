3197d6c401d7d91f4571e550f411da4cc892fe2e
Mellostar25 <67835601+Harsh-Is-Coding@users.noreply.github.com>
2025-09-06 21:06:16 -0400

    new resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Technical Resume - Harsh Shukla.pdf";
+import pdf from "../../Assets/HARSHSHUKLA_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
